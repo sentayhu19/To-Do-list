f6448baa86ffda9a9fcc22f37c01a731e2e769cf
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-03-23 16:40:08 +0300

    console log removed

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,3 +1,4 @@
+import { set } from 'lodash';
 import '../../style.css';
 import Tasks from "./tasklist";
 const tasklist = [];
@@ -24,7 +25,6 @@ const inputBox =document.querySelector("#input-field");
 
 let renderlists = () => {
   const listselector = document.getElementById('to-do-list');
-console.log(tasklist);
   let render = '';
   tasks.list.sort((x, y) => x.index - y.index).forEach((listItem, i) => {
     render += ` <li  id=${i}t class="taskitems"><div class="main-item-wrap"><div class="listitems-wrap"><input type="checkbox" name="checkboxtask" id=${i} class="checkbox" ${tasks.list.completed ? 'checked' : ''} > ${listItem.description}</div><img src="https://icon-library.com/images/three-vertical-dots-icon/three-vertical-dots-icon-6.jpg" alt="edit"></div><hr></li>  `;
@@ -36,19 +36,19 @@ renderlists();
 const clearbtn = document.querySelector('.clear');
 clearbtn.addEventListener('click',()=>{
   let test=[];
-  console.log("clear clicked");
 const checkbox = document.querySelectorAll('input[type=checkbox]:checked');
-console.log("After selection then foreach->");
 checkbox.forEach((box)=>{
-  test.push(box.id);
-console.log("showing array ",test)
-  console.log("Inside  checkbox for each")
-    console.log("checked");
-    const elem = box.parentNode;
+  
+    const elem = box.parentNode.parentNode.parentNode;
     const listid = elem.querySelector('input[class=checkbox]').id;
-    console.log("Id is at index: ",listid);
     tasks.removetask(listid);
-    console.log("caling remove in class");
+    test.push(listid);
     elem.remove();
+    
+});
+tasks.list.forEach((taskindex,i )=> {
+  taskindex.index=i;
+  localStorage.setItem('tasks', JSON.stringify(tasks.list));
+  return;
 });
 });
--- src/assets/js/tasklist.js
+++ src/assets/js/tasklist.js
@@ -6,21 +6,12 @@ export default class Tasks {
     }
   
     removetask(indexval) {
-        console.log("finally remove func called");
-        console.log("ID sent at class is at i ", indexval);
         let i =indexval+'t';
-      this.list = this.list.filter((currentlist) => {
-          document.getElementById(i).style.cssText="display: none;";
-          return currentlist.index != indexval;
-    });
-
-console.log("The whole aray ",this.list);        
-
-      localStorage.setItem('tasks', JSON.stringify(this.list));
+      this.list = this.list.filter((currentlist) => currentlist.index != indexval);
+localStorage.setItem('tasks', JSON.stringify(this.list));   
     }
   
     addtask(task) {
-       
       this.list.push(task);
       localStorage.setItem('tasks', JSON.stringify(this.list));
     }
